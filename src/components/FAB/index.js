71d12b0e455dcfd96f387f9d1a28f4be58736eca
revi <revi@sangkuriang.co.id>
2022-11-26 03:50:38 +0700

    add alt to img

--- src/components/FAB/index.js
+++ src/components/FAB/index.js
@@ -7,7 +7,7 @@ const FAB = ({onClick}) => {
             className="fixed right-10 bottom-10 rounded-full shadow-lg bg-white w-16 h-16 flex flex-col justify-center items-center hover:bg-violet-50"
             onClick={onClick}
         >
-            <img src={fileIcons} className="w-12 h-12" />
+            <img src={fileIcons} className="w-12 h-12" alt="icons" />
         </button>
     )
 }
